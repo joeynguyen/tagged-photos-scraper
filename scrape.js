fb3bc15556697263fb475cd77f43ca30e6034e54
Joey Nguyen <jnn193@gmail.com>
2019-01-04 01:01:14 -0600

    Got filename from image src

--- scrape.js
+++ scrape.js
@@ -5,62 +5,71 @@ const request = require('request');
 require('dotenv').config();
 
 async function download(uri, filename, callback) {
-  request.head(uri, () => {
+  console.log(`Downloading ${filename}`);
+  await request.head(uri, () => {
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 }
 
 async function downloadAllPhotos(page, $photos) {
-  for (const $photo of $photos) {
-    /* eslint-disable no-underscore-dangle */
-    const imageName = $photo._remoteObject.description;
-    /* eslint-disable no-await-in-loop */
-    await $photo.click();
-    // don't need to keep reference in memory
-    $photo.dispose();
-    // 4 second delay for Facebook to figure out appropriate
-    // image size to display based on the browser resolution
-    await page.waitFor(4000);
-    await page.waitForSelector('.fbPhotoSnowliftContainer');
+  const $photo = $photos[0];
 
-    const imageSrc = await page.$eval('.fbPhotoSnowliftContainer img.spotlight', el => el.src);
-    await page.waitFor(2000);
+  // for (const $photo of $photos) {
+  /* eslint-disable no-await-in-loop */
+  await $photo.click();
+  // don't need to keep reference in memory
+  $photo.dispose();
+  await page.waitForSelector('.fbPhotoSnowliftPopup');
+  // 4 second delay for Facebook to figure out appropriate
+  // image size to display based on the browser resolution
+  await page.waitFor(4000);
 
-    await download(imageSrc, `${imageName}.png`, async () => {
-      console.log(`done downloading ${imageName}.png`);
-      await page.waitFor(2000);
+  const imageSrc = await page.$eval('.fbPhotoSnowliftPopup img.spotlight', el => el.src);
+
+  // grab filename of image from URL
+  const reg = /[a-zA-Z_0-9]*\.[a-zA-Z]{3,4}(?=\?)/;
+  const filename = reg.exec(imageSrc)[0];
+  await page.waitFor(2000);
+
+  await download(imageSrc, filename, async () => {
+    try {
+      console.log(`Downloaded ${filename} successfully`);
       await page.keyboard.press('Escape');
-    });
-  }
+      console.log('Escape pressed');
+
+      await page.waitFor(2000);
+      console.log('waited 2 secs again');
+    } catch (err) {
+      console.log('err', err);
+    }
+  });
+  // }
 }
 
 async function scrapeInfiniteScrollPhotos(
   page,
   scrollDelay = 1000,
 ) {
-  let $taggedPhotos = await page.$$('ul.fbPhotosRedesignBorderOverlay > li > a');
-  console.log(`taggedPhotos initial length: ${$taggedPhotos.length}`);
+  // try {
+  //   let previousHeight = 0;
+  //   let currentHeight = await page.evaluate('document.body.scrollHeight');
 
-  try {
-    let previousHeight = 0;
-    let currentHeight = await page.evaluate('document.body.scrollHeight');
+  //   // keep scrolling to the bottom of the page until there are no more photos to load
+  //   while (previousHeight < currentHeight) {
+  //     previousHeight = currentHeight;
+  //     await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+  //     await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+  //     currentHeight = await page.evaluate('document.body.scrollHeight');
+  //     await page.waitFor(scrollDelay);
+  //   }
+  // } catch (e) {
+  //   // there will be an error thrown once the page can't scroll down any further
+  //   // since there aren't any more photos left to load
+  //   // do nothing with the error
+  // }
 
-    // keep scrolling to the bottom of the page until there are no more photos to load
-    while (previousHeight < currentHeight) {
-      previousHeight = currentHeight;
-      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-      currentHeight = await page.evaluate('document.body.scrollHeight');
-      await page.waitFor(scrollDelay);
-    }
-  } catch (e) {
-    // there will be an error thrown once the page can't scroll down any further
-    // since there aren't any more photos left to load
-    // do nothing with the error
-  }
-
-  $taggedPhotos = await page.$$('ul.fbPhotosRedesignBorderOverlay > li > a');
-  console.log(`taggedPhotos FINAL length: ${$taggedPhotos.length}`);
+  const $taggedPhotos = await page.$$('ul.fbPhotosRedesignBorderOverlay > li > a');
+  console.log(`Found ${$taggedPhotos.length} tagged photos`);
 
   await downloadAllPhotos(page, $taggedPhotos);
 }
@@ -68,20 +77,22 @@ async function scrapeInfiniteScrollPhotos(
 (async () => {
   // start puppeteer
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {
-      width: 1600,
-      height: 940,
+      width: 3440,
+      height: 1440,
     },
   });
 
   // Go to website
+  console.log('Going to facebook.com');
   const page = await browser.newPage();
   await page.goto('https://www.facebook.com');
   const context = browser.defaultBrowserContext();
   await context.overridePermissions('https://www.facebook.com', ['notifications']);
 
   // Submit login
+  console.log('Logging in');
   page.focus('#email');
   await page.keyboard.type(process.env.USERNAME);
   const $passField = await page.$('input#pass');
@@ -89,6 +100,7 @@ async function scrapeInfiniteScrollPhotos(
   await $passField.press('Enter');
 
   // Go to "Photos of You" page
+  console.log('Going to "Photos of You" page');
   const $profileLink = await page.waitFor('div[data-click="profile_icon"] a');
   await $profileLink.click();
   const $photosLink = await page.waitFor('a[data-tab-key="photos"]');
@@ -97,10 +109,16 @@ async function scrapeInfiniteScrollPhotos(
   await page.waitFor(1000);
 
   // scrape photos
+  console.log('Searching for photos');
   await scrapeInfiniteScrollPhotos(page);
 
   // stop puppeteer
-  await page.waitFor(1000);
-  console.log('closing browser');
+  await page.waitFor(5000);
+  console.log('waited 5 sec');
+  console.log('Shutting down');
+  await page.close();
+  console.log('page closed');
+
   await browser.close();
+  console.log('browser closed');
 })();
