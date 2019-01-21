adde98743fbc840847784768a536bdf25a02f0f6
Joey Nguyen <jnn193@gmail.com>
2019-01-21 01:35:15 -0600

    Initial update to use mobile site. Able to download first photo in album

--- public/scrape.js
+++ public/scrape.js
@@ -45,21 +45,22 @@ async function downloadAllPhotos(
 
   for (let i = photoStartIndex; i < $photos.length; i++) {
     const $photo = $photos[i];
-    /* eslint-disable no-await-in-loop */
     await $photo.click();
-    await page.waitForSelector('.fbPhotoSnowliftPopup');
+    const $fullSizeLink = await page.waitForSelector(
+      '[href^="/photo/view_full_size"]',
+      { timeout: 10000 }
+    );
+    /* eslint-disable no-await-in-loop */
+    const newPagePromise = new Promise(x => {
+      return browser.once('targetcreated', target => x(target.page()));
+    });
+    await $fullSizeLink.click('my-link');
+    const newPage = await newPagePromise;
 
-    // 5 second delay for Facebook to figure out appropriate
-    // image size to display based on the browser resolution
-    await page.waitFor(5000);
     // stop referencing the element handle
     $photo.dispose();
 
-    const imageSrc = await page.$eval(
-      '.fbPhotoSnowliftPopup img.spotlight',
-      el => el.src
-    );
-
+    const imageSrc = await newPage.url();
     // grab filename of image from URL
     const regx = /[a-zA-Z_0-9]*\.[a-zA-Z]{3,4}(?=\?)/;
     let filename = regx.exec(imageSrc)[0];
@@ -69,15 +70,15 @@ async function downloadAllPhotos(
     await downloadFile(imageSrc, filename, i, page, ipc, electronWindow);
 
     // press Escape to hide currently displayed high quality image
-    await page.keyboard.press('Escape');
-    await page.waitFor(2000);
+    await newPage.close();
+    const $backButton = await page.$('[data-sigil="MBackNavBarClick"]');
+    $backButton.click();
   }
 }
 
 async function scrapeInfiniteScrollPhotos(page, ipc, scrollDelay = 1000) {
-  let $taggedPhotos = await page.$$(
-    'ul.fbPhotosRedesignBorderOverlay > li > a'
-  );
+  const photosQuerySelector = '.timeline.photos a';
+  let $taggedPhotos = await page.$$(photosQuerySelector);
   log.info(`Found ${$taggedPhotos.length} photos`);
   ipc.send('photos-found', $taggedPhotos.length);
 
@@ -95,9 +96,7 @@ async function scrapeInfiniteScrollPhotos(page, ipc, scrollDelay = 1000) {
       );
       currentHeight = await page.evaluate('document.body.scrollHeight');
       await page.waitFor(scrollDelay);
-      $taggedPhotos = await page.$$(
-        'ul.fbPhotosRedesignBorderOverlay > li > a'
-      );
+      $taggedPhotos = await page.$$(photosQuerySelector);
       log.info(`Found ${$taggedPhotos.length} photos`);
       ipc.send('photos-found', $taggedPhotos.length);
       log.info('Scrolling down the page to load more photos');
@@ -110,7 +109,7 @@ async function scrapeInfiniteScrollPhotos(page, ipc, scrollDelay = 1000) {
     ipc.send('status-friendly', 'Found all of your tagged photos');
   }
 
-  $taggedPhotos = await page.$$('ul.fbPhotosRedesignBorderOverlay > li > a');
+  $taggedPhotos = await page.$$(photosQuerySelector);
   log.info(`Final count: ${$taggedPhotos.length} tagged photos found.`);
   ipc.send('photos-found', $taggedPhotos.length);
 
@@ -139,108 +138,125 @@ async function main(photoStartIndex, visualModeOptions, ipc, electronWindow) {
   });
 
   // handle errors
-  process.on('uncaughtException', async err => {
-    log.error('process uncaughtException error', err);
-    ipc.send(
-      'status-friendly',
-      `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
-    );
-    ipc.send('status-internal', 'crashed');
-    await page.close();
-  });
-  page.on('pageerror', async err => {
-    log.error('page uncaughtException error', err);
-    ipc.send(
-      'status-friendly',
-      `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
-    );
-    ipc.send('status-internal', 'crashed');
-    await page.close();
-  });
-  page.on('error', async err => {
-    log.error('puppeteer page crash error', err);
-    ipc.send(
-      'status-friendly',
-      `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
-    );
-    ipc.send('status-internal', 'crashed');
-    await page.close();
-  });
-  browser.on('disconnected', async () => {
-    log.warn('puppeteer browser disconnected');
-    await browser.close();
-  });
-  ipcMain.on('stop-scraper', () => {
-    // don't use async/await because we don't want to wait for other processes
-    // immediately shut down puppeteer
-    log.warn('puppeteer received a stop request');
-    ipc.send(
-      'status-friendly',
-      'The scraper was stopped. If you would like to continue downloading where you left off, click the button below.'
-    );
-    ipc.send('status-internal', 'failed');
-    page.close();
-  });
+  // process.on('uncaughtException', async err => {
+  //   log.error('process uncaughtException error', err);
+  //   ipc.send(
+  //     'status-friendly',
+  //     `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
+  //   );
+  //   ipc.send('status-internal', 'crashed');
+  //   await page.close();
+  // });
+  // page.on('pageerror', async err => {
+  //   log.error('page uncaughtException error', err);
+  //   ipc.send(
+  //     'status-friendly',
+  //     `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
+  //   );
+  //   ipc.send('status-internal', 'crashed');
+  //   await page.close();
+  // });
+  // page.on('error', async err => {
+  //   log.error('puppeteer page crash error', err);
+  //   ipc.send(
+  //     'status-friendly',
+  //     `The scraper crashed unexpectedly with an error: ${err}. If you would like to continue downloading where you left off, click the button below.`
+  //   );
+  //   ipc.send('status-internal', 'crashed');
+  //   await page.close();
+  // });
+  // browser.on('disconnected', async () => {
+  //   log.warn('puppeteer browser disconnected');
+  //   await browser.close();
+  // });
+  // ipcMain.on('stop-scraper', () => {
+  //   // don't use async/await because we don't want to wait for other processes
+  //   // immediately shut down puppeteer
+  //   log.warn('puppeteer received a stop request');
+  //   ipc.send(
+  //     'status-friendly',
+  //     'The scraper was stopped. If you would like to continue downloading where you left off, click the button below.'
+  //   );
+  //   ipc.send('status-internal', 'failed');
+  //   page.close();
+  // });
 
   // navigate to Facebook
   log.info('Going to facebook.com');
   ipc.send('status-friendly', 'Going to facebook.com');
-  await page.goto('https://www.facebook.com');
+  await page.goto('https://m.facebook.com');
   const context = browser.defaultBrowserContext();
-  await context.overridePermissions('https://www.facebook.com', [
-    'notifications',
-  ]);
 
   // Submit login
   log.info('Logging in');
   ipc.send('status-friendly', 'Logging in');
-  page.focus('#email');
+  await page.focus('input[name="email"]');
   await page.keyboard.type(process.env.USERNAME);
-  const $passField = await page.$('input#pass');
+  const $passField = await page.$('input[name="pass"]');
   await $passField.type(process.env.PASSWORD);
   await $passField.press('Enter');
 
-  // Go to Profile page from Homepage
-  let $profileLink;
+  // Bypass FB message to remember user on this browser
+  let $rememberButtonNo;
   try {
-    $profileLink = await page.waitFor('div[data-click="profile_icon"] a', {
-      timeout: 10000,
-    });
+    $rememberButtonNo = await page.waitForSelector(
+      '[href^="/login/save-device/cancel"]',
+      {
+        timeout: 10000,
+      }
+    );
   } catch (e) {
     if (e instanceof TimeoutError) {
       // await page.waitForSelector('#reg-link', { timeout: 10000 })
       await page
-        .waitForSelector(
-          '[href^="https://www.facebook.com/recover/initiate"]',
-          { timeout: 10000 }
-        )
+        .waitForSelector('[aria-label="Did you forget your password?"]', {
+          timeout: 10000,
+        })
         .then(async () => {
           log.error('login credentails incorrect');
           ipc.send('status-internal', 'failed');
           ipc.send('status-friendly', 'The login credentials are incorrect');
         })
         .catch(async () => {
-          log.error("Couldn't find profile_icon selector on homepage");
-          ipc.send('status-internal', 'failed');
-          ipc.send(
-            'status-friendly',
-            'The page is missing a required, expected link.  Please let the developer of this app know about this issue.'
-          );
-        })
-        .finally(async () => {
-          await page.close();
+          await page
+            .waitForSelector('[href^="/reg/"]', { timeout: 10000 })
+            .then(async () => {
+              log.error('login credentails incorrect');
+              ipc.send('status-internal', 'failed');
+              ipc.send('status-friendly', "That account doesn't exist");
+            })
+            .catch(async () => {
+              log.error("Couldn't find profile_icon selector on homepage");
+              ipc.send('status-internal', 'failed');
+              ipc.send(
+                'status-friendly',
+                'The page is missing a required, expected link.  Please let the developer of this app know about this issue.'
+              );
+            })
+            .finally(async () => {
+              await page.close();
+            });
         });
     }
   }
+  await $rememberButtonNo.click();
+  await page.waitForNavigation();
+
+  // Go to Profile page from Homepage
+  log.info('Going to your profile page');
+  ipc.send('status-friendly', 'Going to your profile page');
+  const $profileLink = await page.waitForSelector('#MComposer a', {
+    timeout: 10000,
+  });
   await $profileLink.click();
 
   // Go to "Photos of You" page
-  const $photosLink = await page.waitFor('a[data-tab-key="photos"]');
   log.info('Going to "Photos of You" page');
   ipc.send('status-friendly', 'Going to "Photos of You" page');
-  await $photosLink.click();
-  await page.waitFor('a[name="Photos of You"]');
-  await page.waitFor(1000);
+  const userProfileUrl = await page.url();
+  console.log('userProfileUrl', userProfileUrl);
+
+  await page.goto(`${userProfileUrl}/photos`);
 
   // scrape photos
   log.info('Searching for photos');
