734757aa7fab6a7d473164f06380d2f92aca0a8b
Joey Nguyen <jnn193@gmail.com>
2019-01-22 01:17:43 -0600

    Wrap logic inside downloadAllPhotos for-loop inside try/catch

--- public/scrape.js
+++ public/scrape.js
@@ -17,9 +17,9 @@ function downloadFile(url, filename, iter, page, ipc, electronWindow) {
       ipc.send('photo-number-downloaded', iter);
     })
     .catch(err => {
-      const errMessage = `Downloading failed at photo #${iter} before all photos were downloaded`;
+      const errMessage = `Downloading failed at photo #${iter} before all photos were downloaded. If you would like to continue from the last downloaded photo, click the button below.`;
       log.info(errMessage);
-      log.error('error', err.message);
+      log.error('error', err);
       ipc.send('status-friendly', errMessage);
       ipc.send('status-internal', 'crashed');
       page.close();
@@ -37,43 +37,51 @@ async function downloadAllPhotos(
   ipc.send('status-friendly', 'Downloading photos...');
 
   for (let i = photoStartIndex; i < $photos.length; i++) {
-    const $photo = $photos[i];
-    const hrefPropertyHandle = await $photo.getProperty('href');
-    const photoUrl = await hrefPropertyHandle.jsonValue();
-    const newPhotoPage = await browser.newPage();
-    await newPhotoPage.goto(photoUrl);
+    try {
+      const $photo = $photos[i];
+      const hrefPropertyHandle = await $photo.getProperty('href');
+      const photoUrl = await hrefPropertyHandle.jsonValue();
+      const newPhotoPage = await browser.newPage();
+      await newPhotoPage.goto(photoUrl);
 
-    const $fullSizeLink = await newPhotoPage.waitForSelector(
-      '[href^="/photo/view_full_size"]',
-      { timeout: 10000 }
-    );
-    /* eslint-disable no-await-in-loop */
-    const newPagePromise = new Promise(x => {
-      return browser.once('targetcreated', target => x(target.page()));
-    });
-    await $fullSizeLink.click();
-    const newPage = await newPagePromise;
+      const $fullSizeLink = await newPhotoPage.waitForSelector(
+        '[href^="/photo/view_full_size"]',
+        { timeout: 10000 }
+      );
+      /* eslint-disable no-await-in-loop */
+      const newPagePromise = new Promise(x => {
+        return browser.once('targetcreated', target => x(target.page()));
+      });
+      await $fullSizeLink.click();
+      const newPage = await newPagePromise;
 
-    // having these `.waitFor(1000)` methods help
-    // prevent Puppeteer scraping errors
-    await newPhotoPage.waitFor(1000);
-    await newPhotoPage.close();
-    await newPage.waitFor(1000);
-    const imageSrc = await newPage.url();
-    log.info(`imageSrc: ${imageSrc}`);
+      // having these `.waitFor(1000)` methods help
+      // prevent Puppeteer scraping errors
+      await newPhotoPage.waitFor(1000);
+      await newPhotoPage.close();
+      await newPage.waitFor(1000);
+      const imageSrc = await newPage.url();
 
-    // grab filename of image from URL
-    const regx = /[a-zA-Z_0-9]*\.[a-zA-Z]{3,4}(?=\?)/;
-    let filename = regx.exec(imageSrc)[0];
-    // append index number in front of filename for debugging purposes
-    filename = `${i}-${filename}`;
+      // grab filename of image from URL
+      const regx = /[a-zA-Z_0-9]*\.[a-zA-Z]{3,4}(?=\?)/;
+      let filename = regx.exec(imageSrc)[0];
+      // append index number in front of filename for debugging purposes
+      filename = `${i}-${filename}`;
 
-    await downloadFile(imageSrc, filename, i, page, ipc, electronWindow);
+      await downloadFile(imageSrc, filename, i, page, ipc, electronWindow);
+      await newPage.close();
 
-    await newPage.close();
-
-    // stop referencing the element handle
-    $photo.dispose();
+      // stop referencing the element handle
+      $photo.dispose();
+    } catch (e) {
+      log.error(`error: ${e}`);
+      ipc.send('status-internal', 'failed');
+      ipc.send(
+        'status-friendly',
+        'Downloading failed before all photos were downloaded. If you would like to continue from the last downloaded photo, click the button below.'
+      );
+      await page.close();
+    }
   }
 }
 
