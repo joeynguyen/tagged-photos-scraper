bfdb521acf2386c984c7b95563e012314cd216d9
Joey Nguyen <jnn193@gmail.com>
2019-01-29 01:03:16 -0600

    Add auto-update functionality and release script

--- public/electron.js
+++ public/electron.js
@@ -4,6 +4,7 @@ const { app, BrowserWindow, ipcMain, Menu, session } = electron;
 const path = require('path');
 const isDev = require('electron-is-dev');
 const log = require('electron-log');
+const { autoUpdater } = require('electron-updater');
 const unhandled = require('electron-unhandled');
 
 const scrape = require('./lib/scrape.js');
@@ -143,6 +144,8 @@ function createMenu() {
 app.on('ready', async () => {
   if (isDev) {
     await installExtensions();
+  } else {
+    autoUpdater.checkForUpdatesAndNotify();
   }
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     callback({
@@ -177,6 +180,7 @@ app.on('activate', function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
 ipcMain.on(
   'run-scraper',
   (event, username, password, photoStartIndex, visualMode) => {
